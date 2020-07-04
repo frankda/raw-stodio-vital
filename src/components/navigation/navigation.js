33be3c6c69ad7818734c7e71c28170bc7bc8a9af
frankda <36907108+frankda@users.noreply.github.com>
2020-07-04 17:24:02 +1000

    navigation bar finished

---
+++ src/components/navigation/navigation.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import './navigation.scss';
+
+const Navigation = () => {
+  const links = [
+    {
+      linkName: 'About',
+      linkHref: '#'
+    },
+    {
+      linkName: 'Practitioners',
+      linkHref: '#'
+    },
+    {
+      linkName: 'Our Products',
+      linkHref: '#'
+    },
+  ]
+
+  return (
+    <header>
+      <div className="brand">
+        <div className="brand-logo">
+          <p>v.</p>
+        </div>
+        <div className="brand-name">
+          <span>vital.ly</span>
+        </div>
+      </div>
+
+      <div className="nav-links">
+        <nav>
+          {links.map((link, i) => {
+            return (
+              <a key={i} href={link.linkHref}>{link.linkName}</a>
+            )
+          })}
+        </nav>
+      </div>
+
+      <div className="buttons">
+          <button className="signin">Sign in</button>
+          <button className="create-account">Create an account</button>
+      </div>
+    </header>
+  )
+}
+
+export default Navigation;
