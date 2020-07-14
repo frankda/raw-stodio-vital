6600d66dbd9a4a6820d43ed0fcc3ce96504a68d0
frankda <36907108+frankda@users.noreply.github.com>
2020-07-14 22:50:34 +1000

    apply react-slick

--- src/components/dicover-our-hub/discoverOurHubSection.js
+++ src/components/dicover-our-hub/discoverOurHubSection.js
@@ -5,6 +5,11 @@ import BlogCard from './blogCard/blogCard';
 import FirstPersonPhoto from './img/p1.png';
 import SecondPersonPhoto from './img/p2.png';
 import BannerPhoto from './img/big.png';
+import Slider from 'react-slick';
+// import './slicker.scss';
+// import './slickerTheme.scss';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export const DiscoverOurHub = () => {
   const cardInfo = {
@@ -12,6 +17,16 @@ export const DiscoverOurHub = () => {
     secondPhoto: SecondPersonPhoto,
     bannerPhoto: BannerPhoto
   }
+
+  let settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true
+  };
 
   return (
     <div className="discover-our-hub-section">
@@ -29,11 +44,31 @@ export const DiscoverOurHub = () => {
        </div>
 
        <div className="discover-our-hub-blog">
-        <BlogCard cardInfo={cardInfo} />
-        <BlogCard cardInfo={cardInfo} />
-        <BlogCard cardInfo={cardInfo} />
-        <BlogCard cardInfo={cardInfo} />
+          <BlogCard cardInfo={cardInfo} />
+          <BlogCard cardInfo={cardInfo} />
+          <BlogCard cardInfo={cardInfo} />
+          <BlogCard cardInfo={cardInfo} />
        </div>
+       <Slider {...settings}>
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+        </Slider>
      </div>
     </div>
   )
