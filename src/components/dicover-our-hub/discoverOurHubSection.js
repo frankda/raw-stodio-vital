3e1cc74dd7e5b5a3dcb94e9e77a803581bf4ac81
frankda <suyunyi@gmail.com>
2020-07-16 22:20:22 +1000

    install greensock package

--- src/components/dicover-our-hub/discoverOurHubSection.js
+++ src/components/dicover-our-hub/discoverOurHubSection.js
@@ -51,26 +51,6 @@ export const DiscoverOurHub = () => {
           <BlogCard cardInfo={cardInfo} />
         </Slider>
        </div>
-       {/* <Slider {...settings}>
-          <div>
-            <h3>1</h3>
-          </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
-          </div>
-        </Slider> */}
      </div>
     </div>
   )
